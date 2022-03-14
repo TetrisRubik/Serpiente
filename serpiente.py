ae49af22476b9d6185926c43140c8c6c65610599
TetrisRubik <fpablo960@gmail.com>
2022-03-15 00:22:30 +0100

    Creación de la clase serpiente Se añade la clase serpiente, que es el jugador. Incluye el constructor y el movimiento, así como los controles principales.

--- serpiente.py
+++ serpiente.py
@@ -26,3 +26,55 @@ class cubo(object):
 			circulo_medio2 = (i*dis+dis-radio*2, j*dis+8)
 			pygame.draw.circle(superficie, (0,0,0), circulo_medio, radio)
 			pygame.draw.circle(superficie, (0,0,0), circulo_medio2, radio)
+
+class serpiente(object):
+	cuerpo = []
+	turnos = {}
+
+	def __init__(self, color, pos):
+		self.color = color
+		self.cabeza = cubo(pos)
+		self.cuerpo.append(self.cabeza)
+		self.dirnx = 0
+		self.dirny = 1
+
+	def mueve(self):
+		for evento in pygame.event.get():
+			if evento.type == pygame.QUIT:
+				pygame.quit()
+			teclas = pygame.key.get_pressed()
+			for tecla in teclas:
+				if teclas[pygame.K_LEFT]:
+					self.dirnx = -1
+					self.dirny = 0
+					self.turnos[self.cabeza.pos[:]] = [self.dirnx, self.dirny]
+				elif teclas[pygame.K_RIGHT]:
+					self.dirnx = 1
+					self.dirny = 0
+					self.turnos[self.cabeza.pos[:]] = [self.dirnx, self.dirny]
+				elif teclas[pygame.K_UP]:
+					self.dirnx = 0
+					self.dirny = -1
+					self.turnos[self.cabeza.pos[:]] = [self.dirnx, self.dirny]
+				elif teclas[pygame.K_DOWN]:
+					self.dirnx = 0
+					self.dirny = 1
+					self.turnos[self.cabeza.pos[:]] = [self.dirnx, self.dirny]
+		for i, c in enumerate(self.cuerpo):
+			p = c.pos[:]
+			if p in self.turnos:
+				turno = self.turnos[p]
+				c.mueve(turno[0], turno[1])
+				if i == len(self.cuerpo)-1:
+					self.turnos.pop(p)
+			else:
+				if c.dirnx == -1 and c.pos[0] <= 0:
+					c.pos = (c.filas-1, c.pos[1])
+				elif c.dirnx == 1 and c.pos[0] >= c.filas-1:
+					c.pos = (0, c.pos[1])
+				elif c.dirny == 1 and c.pos[1] >= c.filas-1:
+					c.pos = (c.pos[0], 0)
+				elif c.dirny == -1 and c.pos[1] <= 0:
+					c.pos = (c.pos[0], c.filas-1)
+				else:
+					c.mueve(c.dirnx, c.dirny)
