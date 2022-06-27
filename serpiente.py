0fb3b85ffcaa3b10c725d681ddbbd2ad400931d4
TetrisRubik <fpablo960@gmail.com>
2022-06-27 01:55:05 +0100

    Renombramiento Variables más claras y nítidas.

--- serpiente.py
+++ serpiente.py
@@ -7,26 +7,26 @@ class cubo(object):
 	filas = 20
 	a = 500
 	def __init__(self, empieza, dirnx=1, dirny=0, color=(255,0,0)):
-		self.pos = empieza
+		self.posición = empieza
 		self.dirnx = dirnx
 		self.dirny = dirny
 		self.color = color
 
-	def mueve(self, dirnx, dirny):
+	def mover(self, dirnx, dirny):
 		self.dirnx = dirnx
 		self.dirny = dirny
-		self.pos = (self.pos[0] + self.dirnx, self.pos[1] + self.dirny)
+		self.posición = (self.posición[0] + self.dirnx, self.posición[1] + self.dirny)
 
-	def dibuja(self, superficie, ojos=False):
-		dis = self.a // self.filas
-		i = self.pos[0]
-		j = self.pos[1]
-		pygame.draw.rect(superficie, self.color, (i*dis+1, j*dis+1, dis-2, dis-2))
+	def dibujar(self, superficie, ojos=False):
+		distancia = self.a // self.filas
+		i = self.posición[0]
+		j = self.posición[1]
+		pygame.draw.rect(superficie, self.color, (i*distancia+1, j*distancia+1, distancia-2, distancia-2))
 		if ojos:
-			centro = dis // 2
+			centro = distancia // 2
 			radio = 3
-			circulo_medio = (i*dis+centro-radio, j*dis+8)
-			circulo_medio2 = (i*dis+dis-radio*2, j*dis+8)
+			circulo_medio = (i*distancia+centro-radio, j*distancia+8)
+			circulo_medio2 = (i*distancia+distancia-radio*2, j*distancia+8)
 			pygame.draw.circle(superficie, (0,0,0), circulo_medio, radio)
 			pygame.draw.circle(superficie, (0,0,0), circulo_medio2, radio)
 
@@ -50,37 +50,37 @@ class serpiente(object):
 				if teclas[pygame.K_LEFT]:
 					self.dirnx = -1
 					self.dirny = 0
-					self.turnos[self.cabeza.pos[:]] = [self.dirnx, self.dirny]
+					self.turnos[self.cabeza.posición[:]] = [self.dirnx, self.dirny]
 				elif teclas[pygame.K_RIGHT]:
 					self.dirnx = 1
 					self.dirny = 0
-					self.turnos[self.cabeza.pos[:]] = [self.dirnx, self.dirny]
+					self.turnos[self.cabeza.posición[:]] = [self.dirnx, self.dirny]
 				elif teclas[pygame.K_UP]:
 					self.dirnx = 0
 					self.dirny = -1
-					self.turnos[self.cabeza.pos[:]] = [self.dirnx, self.dirny]
+					self.turnos[self.cabeza.posición[:]] = [self.dirnx, self.dirny]
 				elif teclas[pygame.K_DOWN]:
 					self.dirnx = 0
 					self.dirny = 1
-					self.turnos[self.cabeza.pos[:]] = [self.dirnx, self.dirny]
+					self.turnos[self.cabeza.posición[:]] = [self.dirnx, self.dirny]
 		for i, c in enumerate(self.cuerpo):
-			p = c.pos[:]
+			p = c.posición[:]
 			if p in self.turnos:
 				turno = self.turnos[p]
-				c.mueve(turno[0], turno[1])
+				c.mover(turno[0], turno[1])
 				if i == len(self.cuerpo)-1:
 					self.turnos.pop(p)
 			else:
-				if c.dirnx == -1 and c.pos[0] <= 0:
-					c.pos = (c.filas-1, c.pos[1])
-				elif c.dirnx == 1 and c.pos[0] >= c.filas-1:
-					c.pos = (0, c.pos[1])
-				elif c.dirny == 1 and c.pos[1] >= c.filas-1:
-					c.pos = (c.pos[0], 0)
-				elif c.dirny == -1 and c.pos[1] <= 0:
-					c.pos = (c.pos[0], c.filas-1)
+				if c.dirnx == -1 and c.posición[0] <= 0:
+					c.posición = (c.filas-1, c.posición[1])
+				elif c.dirnx == 1 and c.posición[0] >= c.filas-1:
+					c.posición = (0, c.posición[1])
+				elif c.dirny == 1 and c.posición[1] >= c.filas-1:
+					c.posición = (c.posición[0], 0)
+				elif c.dirny == -1 and c.posición[1] <= 0:
+					c.posición = (c.posición[0], c.filas-1)
 				else:
-					c.mueve(c.dirnx, c.dirny)
+					c.mover(c.dirnx, c.dirny)
 
 	def reposicionamiento(self, pos):
 		self.cabeza = cubo(pos)
@@ -90,28 +90,28 @@ class serpiente(object):
 		self.dirnx = 0
 		self.dirny = 1
 
-	def añade_cubo(self):
+	def crecer(self):
 		cola = self.cuerpo[-1]
 		dx, dy = cola.dirnx, cola.dirny
 		if dx == 1 and dy == 0:
-			self.cuerpo.append(cubo((cola.pos[0]-1,cola.pos[1])))
+			self.cuerpo.append(cubo((cola.posición[0]-1,cola.posición[1])))
 		elif dx == -1 and dy == 0:
-			self.cuerpo.append(cubo((cola.pos[0]+1,cola.pos[1])))
+			self.cuerpo.append(cubo((cola.posición[0]+1,cola.posición[1])))
 		elif dx == 0 and dy == 1:
-			self.cuerpo.append(cubo((cola.pos[0],cola.pos[1]-1)))
+			self.cuerpo.append(cubo((cola.posición[0],cola.posición[1]-1)))
 		elif dx == 0 and dy == -1:
-			self.cuerpo.append(cubo((cola.pos[0],cola.pos[1]+1)))
+			self.cuerpo.append(cubo((cola.posición[0],cola.posición[1]+1)))
 		self.cuerpo[-1].dirnx = dx
 		self.cuerpo[-1].dirny = dy
 
-	def dibuja(self, superficie):
+	def dibujar(self, superficie):
 		for i, c in enumerate(self.cuerpo):
 			if i == 0:
-				c.dibuja(superficie, True)
+				c.dibujar(superficie, True)
 			else:
-				c.dibuja(superficie)
+				c.dibujar(superficie)
 
-def dibuja_reja(a, filas, superficie):
+def dibujar_reja(a, filas, superficie):
 	tamaño_entre = a // filas
 	x = 0
 	y = 0
@@ -122,19 +122,19 @@ def dibuja_reja(a, filas, superficie):
 		pygame.draw.line(superficie, (255,255,255), (0,y), (a,y))
 
 def dibuja_ventana(superficie):
-	global filas, anchura, s, fruta
+	global filas, ancho, serpiente, fruta
 	superficie.fill((0,0,0))
-	s.dibuja(superficie)
-	fruta.dibuja(superficie)
-	dibuja_reja(anchura, filas, superficie)
+	serpiente.dibujar(superficie)
+	fruta.dibujar(superficie)
+	dibujar_reja(ancho, filas, superficie)
 	pygame.display.update()
 
-def pon_fruta(filas, item):
-	pos = item.cuerpo
+def poner_fruta(filas, item):
+	posición = item.cuerpo
 	while True:
 		x = random.randrange(filas)
 		y = random.randrange(filas)
-		if len(list(filter(lambda z:z.pos == (x,y), pos))) > 0:
+		if len(list(filter(lambda x:x.posición == (x,y), posición))) > 0:
 			continue
 		else:
 			break
@@ -150,26 +150,26 @@ def caja_mensaje(sujeto, contenido):
 	except:
 		pass
 
-def main():
-	global anchura, filas, s, fruta
-	anchura = 500
+def principal():
+	global ancho, filas, serpiente, fruta
+	ancho = 500
 	filas = 20
-	ventana = pygame.display.set_mode((anchura, anchura))
-	s = serpiente((255,0,0), (10,10))
-	fruta = cubo(pon_fruta(filas, s), color=(0,255,0))
+	ventana = pygame.display.set_mode((ancho, ancho))
+	serpiente = serpiente((255,0,0), (10,10))
+	fruta = cubo(poner_fruta(filas, serpiente), color=(0,255,0))
 	reloj = pygame.time.Clock()
-	bandera = True
-	while bandera:
+	en_juego = True
+	while en_juego:
 		pygame.time.delay(50)
 		reloj.tick(10)
-		s.mueve()
-		if s.cuerpo[0].pos == fruta.pos:
-			s.añade_cubo()
-			fruta = cubo(pon_fruta(filas, s), color=(0,255,0))
-		if s.cuerpo[0].pos in list(map(lambda z:z.pos,s.cuerpo[1:])):
-			print("Score: ", len(s.cuerpo))
+		serpiente.mueve()
+		if serpiente.cuerpo[0].posición == fruta.posición:
+			serpiente.crecer()
+			fruta = cubo(poner_fruta(filas, serpiente), color=(0,255,0))
+		if serpiente.cuerpo[0].posición in list(map(lambda x:x.posición,serpiente.cuerpo[1:])):
+			print("Puntuación: ", len(serpiente.cuerpo))
 			caja_mensaje("¡Perdiste!", "¿Nuevo intento...?")
-			s.reposicionamiento((10,10))
+			serpiente.reposicionamiento((10,10))
 		dibuja_ventana(ventana)
 
-main()
+principal()
